bc96bfedd6496729a1b7a01382143be311ab6798
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-06-27 18:50:31 +0100

    Wire up keyup event on the input field

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const emailField = document.getElementById('email');
 const passwordField = document.getElementById('password');
 
 console.log(nameField, emailField, passwordField)
+
+nameField.addEventListener('keyup', () => {
+  console.log('NAME FIELD CHANGED')
+})
