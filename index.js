ac3bb970779607fa65caffd68f990a91d717ee94
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-06-27 18:53:39 +0100

    Wire up name validation to name field

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const nameField = document.getElementById('name');
 const emailField = document.getElementById('email');
 const passwordField = document.getElementById('password');
+const nameErrorDiv = document.getElementById('name-errors')
+const emailErrorDiv = document.getElementById('email-errors')
+const passwordErrorDiv = document.getElementById('password-errors')
 
 console.log(nameField, emailField, passwordField)
 
 nameField.addEventListener('keyup', () => {
-  console.log('NAME FIELD CHANGED')
+  const name = nameField.value
+  if (name.length < 3) {
+    passwordErrorDiv.innerText = 'Name must be at least 3 characters long'
+  } else {
+    passwordErrorDiv.innerText = ''
+  }
 })
